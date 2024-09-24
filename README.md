dbb964d8f027e05b86382c19d34802d1d35cdfa1
dangar12345 <garcianaranjodaniel@gmail.com>
2024-09-24 10:17:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# dontorrent-plugin
+dontorrent search plugin for qbittorrent
+
+# Installation
+
+First copy the raw [link](https://raw.githubusercontent.com/dangar16/dontorrent-plugin/main/dontorrent.py) and follow the next steps:
+
+1- Enable search engine
+![image](https://github.com/dangar16/dontorrent-plugin/assets/95357264/d0a2cb24-7ce4-44a0-b0f0-a642fe01e02e)
+
+2- Click on search tab
+![image](https://github.com/dangar16/dontorrent-plugin/assets/95357264/43468e5e-259c-44ec-bdbb-f9a52d3542cc)
+
+3- Add the url
+![image](https://github.com/dangar16/dontorrent-plugin/assets/95357264/0036a8e0-e695-4fd5-8120-94b413f4ba06)
+
+4- Search
+
+![image](https://github.com/dangar16/dontorrent-plugin/assets/95357264/268dd9a6-8e34-4a0f-bb9f-172c76effc13)
+
+5- Download Torrent
+
+For some reason that I don't know, if I double click in a entry it doesn't add the torrent to download. So if you want to download the torrent follow the next steps
+
+![image](https://github.com/dangar16/dontorrent-plugin/assets/95357264/6d95d566-1be9-41f9-887b-041a6fdad71a)
+
+Click below file (Archivo) and press download
+![image](https://github.com/dangar16/dontorrent-plugin/assets/95357264/68f2d405-66b1-48c7-8a63-40138c46f86e)
+![image](https://github.com/dangar16/dontorrent-plugin/assets/95357264/334558c2-9771-40fb-9b65-88f815162e52)
+![image](https://github.com/dangar16/dontorrent-plugin/assets/95357264/bafa8f74-967e-4a42-8287-d1d3dad73b90)
+
+
+# Info
+
+Sometimes, when performing a search, it does not show results and this message appears
+
+![image](https://github.com/dangar16/dontorrent-plugin/assets/95357264/ac4adcb8-c3ed-413d-a8aa-d84b49a171df)
+
+It can also happen when you want to advance the page in the search, so I recommend not doing very general searches.
+
+The seeders and leechers are -1 because dontorrent doesn't include in the page that info
---
+++ pediatorrent.py
@@ -0,0 +1,134 @@
+#VERSION: 1.00
+# AUTHORS: Daniel Naranjo (garcianaranjodaniel@gmail.com)
+# LICENSING INFORMATION
+
+from helpers import download_file, retrieve_url
+from novaprinter import prettyPrinter
+# some other imports if necessary
+import re
+
+class pediatorrent(object):
+    """
+    `url`, `name`, `supported_categories` should be static variables of the engine_name class,
+     otherwise qbt won't install the plugin.
+
+    `url`: The URL of the search engine.
+    `name`: The name of the search engine, spaces and special characters are allowed here.
+    `supported_categories`: What categories are supported by the search engine and their corresponding id,
+    possible categories are ('all', 'anime', 'books', 'games', 'movies', 'music', 'pictures', 'software', 'tv').
+    """
+
+    url = 'https://pediatorrent.com/'
+    name = 'PediaTorrent'
+    supported_categories = {
+        'all': '',
+    }
+
+    def __init__(self):
+        """
+        Some initialization
+        """
+
+    def download_torrent(self, url):
+        """
+        Providing this function is optional.
+        It can however be interesting to provide your own torrent download
+        implementation in case the search engine in question does not allow
+        traditional downloads (for example, cookie-based download).
+        """
+        
+        """
+        Esto es paja
+        try:
+            file, url = download_file(url).split(" ")
+            torrent = open(file, 'rb').read()
+            metadata = bencode.bdecode(torrent)
+            hashcontents = bencode.bencode(metadata['info'])
+            digest = hashlib.sha1(hashcontents).digest()
+            b32hash = base64.b32encode(digest)
+        except Exception as e:
+            return ""
+
+        return "magnet:?xt=urn:btih:" + str(b32hash)[2:-1]
+        """
+        return download_file(url)
+        
+
+    # DO NOT CHANGE the name and parameters of this function
+    # This function will be the one called by nova2.py
+    def search(self, what, cat='all'):
+        """
+        Here you can do what you want to get the result from the search engine website.
+        Everytime you parse a result line, store it in a dictionary
+        and call the prettyPrint(your_dict) function.
+
+        `what` is a string with the search tokens, already escaped (e.g. "Ubuntu+Linux")
+        `cat` is the name of a search category in ('all', 'anime', 'books', 'games', 'movies', 'music', 'pictures', 'software', 'tv')
+        """
+
+        search_url = f"{self.url}buscar?q={what.replace('+','%20')}"
+        html = retrieve_url(search_url)
+
+        """
+        Para saber si ha encontrado torrents, en dontorrent te muestra un mensaje con la cantidad de torrents encontrados
+        Si no encuentra nada, la cantidad de torrents es 0
+        """
+        quantity = re.findall(r'<p.*?class="text-2xl text-lime-500 text-center.*?</p>', html)
+        coincidencias = re.findall(r'\d+', quantity[0])
+        
+        quantity = int(coincidencias[2])
+        """
+        En la paginación tiene dos botones que son para ir a la pagina anterior y a la siguiente
+        Por ello uso pages[1:-1] para quitar esos dos botones
+        """
+        pages = quantity // 17 + 1
+        
+        links = []
+
+        """
+        Las páginas tienen la siguiente estructura: url/buscar/what/page/[0-pages]
+        Por ejemplo: https://dontorrent.cologne/buscar/star%20wars/page/1
+        """
+        for i in range(1, pages + 1):
+            url = f"{self.url}buscar/page/{i}?q={what.replace('+','%20')}"
+            html = retrieve_url(url)
+            a_list = re.findall(r'<a.*?>', html)
+            a_list = a_list[len(a_list) - quantity:-2]
+            for a in a_list:
+                url = re.findall(r'href=[\'"]?([^\'" >]+)', a)
+                if len(url) > 0:
+                    links.append(url[0])
+        for i in links:
+            try: 
+                html = retrieve_url(i)
+                item = {}
+                item['seeds'] = '-1'
+                item['leech'] = '-1'
+                item['engine_url'] = self.url
+                item['desc_link'] = i
+                item['name'] = i.split("/")[-1].replace("-", " ")
+                name=i.split("/")[-1].replace("-", " ")
+                tipo = i.split("/")[3]
+                if tipo != "series":
+                    a = re.findall(r'<a.*?>', html)
+                    a = a[11:12]
+                    url = re.findall(r'href=[\'"]?([^\'" >]+)', a[0])[0]
+                    item['link'] = url
+                    item['size'] = -1
+                    prettyPrinter(item)
+                else:
+                    tds = re.findall(r'<td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium ml-auto">(.*?)</td>', html, re.M|re.I|re.S)
+                    for td in tds:
+                        td_link = re.findall(r'href=[\'"]?([^\'" >]+)', td, re.DOTALL)
+                        a = td_link[0]
+
+                        try:
+                            download_link = a
+                            item['link'] = download_link
+                            item['size'] = -1
+                            item['name'] = name + " " + td_link[0]
+                            prettyPrinter(item)
+                        except Exception:
+                            continue
+            except Exception:
+                continue
